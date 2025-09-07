8703280bf55727778fb0509c8001f084f5ec4224
m00n-armando <thegreatzybon@gmail.com>
2025-09-08 02:10:15 +0700

    Add Vercel Analytics to track website usage and performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "./context/LanguageContext";
 import Chatbot from "./components/Chatbot";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <LanguageProvider>
           {children}
           <Chatbot />
+          <Analytics />
         </LanguageProvider>
       </body>
     </html>
